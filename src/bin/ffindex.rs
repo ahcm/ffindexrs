fb6946be1a48999122d1877b6159afc4658eb2e1
Gitea <gitea@fake.local>
2019-11-04 15:06:35 +0100

    Initial import

---
+++ src/bin/ffindex.rs
@@ -0,0 +1,54 @@
+
+
+use std::io;
+
+extern crate clap; 
+use clap::{Arg, App, SubCommand, AppSettings};
+
+use ffindexrs::ffindex_db_open;
+use ffindexrs::ffindex_get_data_by_name;
+
+
+fn ffindex_get(ffindex_path:String, ffdata_path:String, key:String)
+{
+    let ffindex_db = ffindex_db_open(ffindex_path, ffdata_path);
+    match ffindex_get_data_by_name(&ffindex_db, key)
+    {
+        Some(data) => print!("{}", std::str::from_utf8(data).unwrap()),
+        None => println!("Not found")
+    }
+}
+
+
+fn main() -> io::Result<()>
+{
+  let app = App::new("ffindex")
+    .version("0.1")
+    .about("FFindex flat file database")
+    .author("Andreas Hauser")
+    .setting(AppSettings::ArgRequiredElseHelp)
+    .subcommand(SubCommand::with_name("test").about("controls testing features"))
+    .subcommand(SubCommand::with_name("get")
+                .about("ffindex get - extract records")
+                .setting(AppSettings::ArgRequiredElseHelp)
+                .arg(Arg::with_name("ffindex").short("i").takes_value(true).required(true))
+                .arg(Arg::with_name("ffdata").short("d").takes_value(true).required(true))
+                .arg(Arg::with_name("key").short("k").takes_value(true).required(true)))
+  ;
+  let matches = app.get_matches(); 
+
+  match matches.subcommand()
+  {
+      ("test",  Some(arg)) =>  { println!("{:?}", arg) },
+      ("get",   Some(_arg)) =>  {
+          if let Some(submatches) = matches.subcommand_matches("get") {
+              ffindex_get(submatches.value_of("ffindex").expect("ffindex").to_string(),
+                          submatches.value_of("ffdata").expect("ffdata").to_string(),
+                          submatches.value_of("key").expect("key").to_string())
+          }
+      },
+      other => {println!("Command: {:?}", other)}
+  };
+
+  Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use memmap::MmapOptions;
+use std::fs::File;
+
+extern crate csv;
+use csv::{ReaderBuilder};
+
+#[macro_use]
+extern crate serde;
+
+
+pub fn mmap_file(filepath : String) -> memmap::Mmap
+{
+    let file = File::open(filepath).expect("file problem");
+    unsafe { MmapOptions::new().map(&file) }.expect("mmaping failed")
+}
+
+
+#[derive(Debug,Deserialize)]
+pub struct FFindexEntry
+{
+    name: String,
+    offset: usize,
+    length: usize
+}
+
+
+pub struct FFindexDB
+{
+    ffindex_path : String,
+    ffdata_path : String,
+    entries : Vec<FFindexEntry>,
+    ffdata : memmap::Mmap,
+}
+
+
+pub fn load_index(ffindex_index_path:String) -> Vec<FFindexEntry>
+{
+    let mut rdr = ReaderBuilder::new()
+                    .has_headers(false)
+                    .delimiter(b'\t')
+                    .from_path(ffindex_index_path)
+                    .expect("reader");
+
+    rdr.deserialize().collect::<Result<Vec<FFindexEntry>, csv::Error>>().expect("Reading ffindex")
+}
+
+
+pub fn ffindex_db_open(ffindex_path : String, ffdata_path : String) -> FFindexDB
+{
+    FFindexDB
+    {
+        ffindex_path : ffindex_path.clone(),
+        ffdata_path : ffdata_path.clone(),
+        entries : load_index(ffindex_path),
+        ffdata : mmap_file(ffdata_path),
+    }
+}
+
+
+pub fn ffindex_get_entry_by_name<'a>(ffindex_db : &'a FFindexDB, key : String) -> Result<&'a FFindexEntry,usize>
+{
+    match ffindex_db.entries.binary_search_by(|entry| entry.name.cmp(&key))
+    {
+        Ok(i) => Ok(&ffindex_db.entries[i]),
+        Err(i) => Err(i)
+    }
+}
+
+
+pub fn ffindex_get_data_by_name<'a>(ffindex_db : &'a FFindexDB, key : String) ->Option<&'a [u8]>
+{
+    match ffindex_get_entry_by_name(ffindex_db, key)
+    {
+        Ok(entry) => ffindex_db.ffdata.get(entry.offset .. entry.offset + entry.length),
+        Err(_index) => None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::mmap_file;
+
+    #[test]
+    fn it_works() {
+        let filepath = "/etc/passwd";
+        let ffdata = mmap_file(filepath.to_string());
+        println!("{:?}", ffdata.get(0..4));
+    }
+}
